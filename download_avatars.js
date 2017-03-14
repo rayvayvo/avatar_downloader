45cb3da56cc989537425f0a401ff54e4718561c1
Raven Ackerman <rayvayvo@gmail.com>
2017-03-14 20:54:36 +0000

    changed user-agent header

--- download_avatars.js
+++ download_avatars.js
@@ -10,24 +10,36 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //the third being a URL, and downloads all the images from the  URLs
 function getRepoContributors(repoOwner, repoName, cb) {
   // needs work still
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL);
+var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  // request.get('https://api.github.com/repos/jquery/jquery/contributors')
 
-  //   .on('error', function(err) {
-  //     console.log('error, error. danger will robinson!');
-  //   })
-  //   .on('response', function(response) {
-  //     console.log('Response status code: ', response.statusCode);
-  //   })
+var options = {
+  url: requestURL,
+  headers: {
+  'User-Agent': "GitHub Avatar Downloader"
+  }
+};
+// console.log(requestURL);
+// console.log(options);
+
+  request.get(options)
+
+
+
+    .on('error', function(err) {
+      console.log('error, error. danger will robinson!');
+    })
+    .on('response', function(response) {
+      console.log('Response status code: ', response.statusCode);
+      console.log(response.body);
+    })
   //   // .pipe(fs.createWriteStream('./avatars/'));
     };
-
-
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
