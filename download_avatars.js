1736814c583c94ffc45b7eaf7b111a1e6b5fb743
Raven Ackerman <rayvayvo@gmail.com>
2017-03-17 01:22:55 +0000

    now with avatar downloading action.

--- download_avatars.js
+++ download_avatars.js
@@ -9,35 +9,33 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //function that pulls up a list of contributors, pulls out the first 3 keys of each object,
 //the third being a URL, and downloads all the images from the  URLs
 function getRepoContributors(repoOwner, repoName, cb) {
-  // needs work still
-var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-var options = {
-  url: requestURL,
-  headers: {
-  'User-Agent': "GitHub Avatar Downloader"
-  }
-};
-// console.log(requestURL);
-// console.log(options);
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': "GitHub Avatar Downloader"
+    }
+  };
 
   request.get(options, function(error, response, body) {
 
+    if (error) {
       console.log('error, error. danger will robinson!');
+    } else {
+
 
       var info = JSON.parse(response.body);
-      // console.log(info);
-      // console.log(response.body);
       console.log('Response status code: ', response.statusCode);
       for (let avatarURL in info)
-      console.log(info[avatarURL].avatar_url);
+        request.get(info[avatarURL].avatar_url)               // Note 1
+       .pipe(fs.createWriteStream(`./downloaded/${info[avatarURL].login}.png`));
 
-    // })
-  //   // .pipe(fs.createWriteStream('./avatars/'));
-    });
+    }
+
+  });
 };
-
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
